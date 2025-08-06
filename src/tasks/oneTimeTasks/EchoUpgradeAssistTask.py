adc0930f2ef1cc6952cfffacd0d724a6acbdc087
Nefeli <1084272329@qq.com>
2025-08-06 15:31:14 +0800

    feat(task): `声骸升级辅助任务`中加入窗口置顶
    声骸升级辅助任务:
    * 在启动后会自动置顶窗口
    * 加入退出检测
    * 检测已有声骸，去掉了第一次检测
    
    此外发现一个bug，在windows_utils.py中使用get_active_window得到的不是ok脚本的窗口句柄
    
    Signed-off-by: Nefeli <1084272329@qq.com>

--- src/tasks/oneTimeTasks/EchoUpgradeAssistTask.py
+++ src/tasks/oneTimeTasks/EchoUpgradeAssistTask.py
@@ -2,13 +2,18 @@ from ok import BaseTask, Box
 
 from src.Echo import contrast_echo
 from src.echo_scorer import getIdByName
+from src.utils.windows_utils import set_window_topmost, get_active_window
 from src.wuwa_scanner_utils import ocr_echo, is_page
 
 
 class EchoUpgradeAssistTask(BaseTask):
     role = {}
     echos = []
-
+    lost_cost = {
+        1: None,
+        3: None,
+        4: None,
+    }
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = "声骸升级辅助"
@@ -32,9 +37,24 @@ class EchoUpgradeAssistTask(BaseTask):
             "roleListId": getIdByName(self.config.get("选择角色")),
             "ming": 1
         }
-
+        set_window_topmost(get_active_window())
         if is_page(self, "echo_change"):
             self.identifyExistingEcho()
+            for echo in self.echos:
+                self.log_info(
+                    f'声骸: {echo.get_name()}\n评分: {echo.get_score()}\n主词条评分: {echo.get_main_score()}\n副词条评分: {echo.get_sub_score()}')
+                cost = echo.get_cost()
+                sub_score = echo.get_sub_score()
+                if self.lost_cost[cost] is None:
+                    self.lost_cost[cost] = sub_score / len(echo.get_sub_attr_list())
+                else:
+                    self.lost_cost[cost] = min(self.lost_cost[cost], sub_score / len(echo.get_sub_attr_list()))
+            for c, s in self.lost_cost:
+                self.notification(
+                    f's',
+                    f'COST{c} 最低副词条平均分'
+                )
+
             self.log_info("可以开始强化声骸了")
 
         a = True
@@ -44,6 +64,9 @@ class EchoUpgradeAssistTask(BaseTask):
             if is_page(self, "echo_upgrade"):
                 if a:
                     a = not self.upgrade_assist()
+            elif is_page(self, "role_echo"):
+                self.log_info("任务结束，已自动退出")
+                break
             else:
                 a = True
 
@@ -61,33 +84,33 @@ class EchoUpgradeAssistTask(BaseTask):
 
     def identifyExistingEcho(self):
         echos = self.echos
-        self.sleep(0.5)
-        self.click_box(box=Box(500, 700, to_x=501, to_y=701))
-        previous = ocr_echo(self, "echo_change")
+        # self.sleep(0.5)
+        # self.click_box(box=Box(500, 700, to_x=501, to_y=701))
+        # previous = ocr_echo(self, "echo_change")
 
         self.click_box(box=Box(53, 217, to_x=183, to_y=348))
         this = ocr_echo(self, "echo_change")
-        if not contrast_echo(previous, this):
-            previous = this
-            echos.append(this)
+        # if not contrast_echo(previous, this):
+        previous = this
 
         self.click_box(box=Box(66, 437, to_x=169, to_y=542))
         this = ocr_echo(self, "echo_change")
         if not contrast_echo(previous, this):
-            previous = this
+            echos.append(previous)
             echos.append(this)
+            previous = this
 
         self.click_box(box=Box(66, 581, to_x=169, to_y=586))
         this = ocr_echo(self, "echo_change")
         if not contrast_echo(previous, this):
-            previous = this
             echos.append(this)
+            previous = this
 
         self.click_box(box=Box(66, 725, to_x=169, to_y=830))
         this = ocr_echo(self, "echo_change")
         if not contrast_echo(previous, this):
-            previous = this
             echos.append(this)
+            previous = this
 
         self.click_box(box=Box(66, 869, to_x=169, to_y=974))
         this = ocr_echo(self, "echo_change")
